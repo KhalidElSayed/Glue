ab6cd67765091d78e7f8681945c8eda4153c7f90
Pietro Rea <pietrorea@gmail.com>
2012-04-17 13:22:52 -0400

    Initial Commit

---
+++ Glue/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Glue
+//
+//  Created by Pietro Rea on 4/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
